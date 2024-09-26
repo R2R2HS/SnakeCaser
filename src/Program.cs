8c633e1f8f5c430b32902ec07f0b03bd19e22151
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2024-09-26 22:44:54 +0300

    Renamer fix
    Fixed an Issue Where Compiler-Generated Fields/Properties/Classes/Events Could Be Renamed When It Was Not Necessary

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,15 @@
 ﻿using dnlib.DotNet;
 using Newtonsoft.Json.Serialization;
 
-internal class Program
+internal static class Program
 {
 	private const string TEST_ASSEMBLY_PATH = "test.dll";
+	private const char DEFAULT_SEPARATOR = '.';
 
 	private static void Main(string[] args)
 	{
 		string assemblyPath = args.Length == 0 ? TEST_ASSEMBLY_PATH : args[0];
-		string snakedAssemblyPath = Path.Combine(Path.GetDirectoryName(assemblyPath), $"{Path.GetFileNameWithoutExtension(assemblyPath)}_snaked.dll");
+		string snakedAssemblyPath = Path.Combine(Path.GetDirectoryName(assemblyPath), $"{GetSnakedString(Path.GetFileNameWithoutExtension(assemblyPath), DEFAULT_SEPARATOR)}_snaked.dll");
 
 		try
 		{
@@ -26,24 +27,29 @@ internal class Program
 
 	private static ModuleDefMD RenameElements(ModuleDefMD module)
 	{
-		module.Name = GetSnakedString(module.Name, '.');
+		module.Name = GetSnakedString(module.Name, DEFAULT_SEPARATOR);
 
 		foreach (var type in module.Types)
 		{
-			if (type.Namespace == "System.Runtime.CompilerServices" || type.Namespace == "Microsoft.CodeAnalysis")
+			if (IsInBlackListedNamespace(type))
+			{
+				continue;
+			}
+
+			if (HasBlackListedAttribute(type))
 			{
 				continue;
 			}
 
 			type.Name = GetSnakedString(type.Name);
-			type.Namespace = GetSnakedString(type.Namespace, '.');
+			type.Namespace = GetSnakedString(type.Namespace, DEFAULT_SEPARATOR);
 
-			foreach (var parameter in type.GenericParameters)
+			foreach (var parameter in type.GenericParameters.Where(DoesNotHaveBlackListedAttribute))
 			{
 				parameter.Name = GetSnakedString(parameter.Name);
 			}
 
-			foreach (var method in type.Methods)
+			foreach (var method in type.Methods.Where(DoesNotHaveBlackListedAttribute))
 			{
 				method.Name = GetSnakedString(method.Name);
 
@@ -58,17 +64,17 @@ internal class Program
 				}
 			}
 
-			foreach (var field in type.Fields)
+			foreach (var field in type.Fields.Where(DoesNotHaveBlackListedAttribute))
 			{
 				field.Name = GetSnakedString(field.Name);
 			}
 
-			foreach (var property in type.Properties)
+			foreach (var property in type.Properties.Where(DoesNotHaveBlackListedAttribute))
 			{
 				property.Name = GetSnakedString(property.Name);
 			}
 
-			foreach (var evt in type.Events)
+			foreach (var evt in type.Events.Where(DoesNotHaveBlackListedAttribute))
 			{
 				evt.Name = GetSnakedString(evt.Name);
 			}
@@ -77,9 +83,31 @@ internal class Program
 		return module;
 	}
 
+	private static bool IsInBlackListedNamespace(TypeDef type)
+	{
+		return type.Namespace == "System.Runtime.CompilerServices" || type.Namespace == "Microsoft.CodeAnalysis";
+	}
+
+	private static bool DoesNotHaveBlackListedAttribute(IHasCustomAttribute hasCustomAttribute)
+	{
+		return !HasBlackListedAttribute(hasCustomAttribute);
+	}
+
+	private static bool HasBlackListedAttribute(IHasCustomAttribute hasCustomAttribute)
+	{
+		return hasCustomAttribute.CustomAttributes.Find("System.Runtime.CompilerServices.CompilerGeneratedAttribute") != null;
+	}
+
 	private static string GetSnakedString(string input, char separator)
 	{
-		return string.Join(".", input.ToString().Split(separator).Select(GetSnakedString));
+		if (input.Contains(separator))
+		{
+			return string.Join(".", input.ToString().Split(separator).Select(GetSnakedString));
+		}
+		else
+		{
+			return GetSnakedString(input);
+		}
 	}
 
 	private static string GetSnakedString(string input)
